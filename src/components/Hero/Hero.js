8844d343f5b371c8ddd98f25a66da241b906f31c
Tyler <109047657+tyleralegre@users.noreply.github.com>
2023-08-24 13:12:57 -0700

    Update Hero.js

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -13,7 +13,7 @@ const Hero = (props) => (
           My Personal Portfolio
         </SectionTitle>
         <SectionText>
-        The purpose of JavaScript Mastery is to help aspiring and established developers to take their development skills to the next level and build awesome apps.
+        Showcasing my developments within the last year of learning the ins and outs of front end!
         </SectionText>
         <Button onClick={props.handleClick}>Learn More</Button>
       </LeftSection>
